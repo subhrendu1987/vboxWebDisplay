2d22ad36cc853cbae07b87cf2426328ecaf1d0b9
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-10-09 10:56:59 +0530

    modified:   ../Dockerfile-vboxwebsrv 	modified:   vboxwebsrv.tar 	modified:   ../init_run.sh

--- init_run.sh
+++ init_run.sh
@@ -6,5 +6,6 @@ sshHost=$(awk -F@ '{print $2}' <<<$1)
 
 echo "Trying to connect "${sshHost}
 sshpass -p $SSH_PASSWD ssh -o StrictHostKeyChecking=no -p $SSH_PORT $1 "hostname"
+# sshpass -p $SSH_PASSWD ssh -o StrictHostKeyChecking=no -p 22 server2@192.168.150.221 "hostname"
 sshpass -p $SSH_PASSWD ssh -o StrictHostKeyChecking=no -p $SSH_PORT $1 "killall vboxwebsrv"
 sshpass -p $SSH_PASSWD ssh -o StrictHostKeyChecking=no -p $SSH_PORT -L 0.0.0.0:18083:$sshHost:$PORT $1 "killall vboxwebsrv; \"$VBOXWEBSRVPATH\" -p $PORT -A null -H $sshHost"
